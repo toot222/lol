b428ee0260123f4085f8bce7785c900035f263f7
toot222 <150439566+toot222@users.noreply.github.com>
2024-03-17 20:58:54 +0100

    Update discorrd.js

--- discorrd.js
+++ discorrd.js
@@ -1,11 +1,26 @@
-const axios = require('axios');
+const fetch = require('node-fetch');
 
-// Vervang de onderstaande URL door je eigen Discord-webhook URL
-const webhookURL = 'https://discord.com/api/webhooks/1219008140159352963/I07kOVhmJivstq-uZl3ccjvyHrjrnypKiULh73NM7WCcJRF3An7gslX7tR1Tn2_I8-ZL';
+const webhookURL = "https://discord.com/api/webhooks/1219008140159352963/I07kOVhmJivstq-uZl3ccjvyHrjrnypKiULh73NM7WCcJRF3An7gslX7tR1Tn2_I8-ZL";
 
-// Maak een HTTP-verzoek naar de webhook om een bericht te sturen
-axios.post(webhookURL, {
-  content: 'hoi'
-})
-.then(() => console.log('Bericht succesvol verstuurd naar de webhook'))
-.catch(error => console.error('Er is een fout opgetreden bij het versturen van het bericht naar de webhook:', error));
+const sendMessage = async () => {
+  try {
+    const response = await fetch(webhookURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ content: 'Hoi' }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to send message to Discord');
+    }
+
+    console.log('Message sent successfully to Discord');
+  } catch (error) {
+    console.error('Error sending message to Discord:', error);
+  }
+};
+
+// Stuur het bericht eenmaal bij het starten van het script
+sendMessage();
