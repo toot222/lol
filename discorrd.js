320f6416fc5869fc0092a0f1d7aa405083b61b1a
toot222 <150439566+toot222@users.noreply.github.com>
2024-03-17 20:56:26 +0100

    Update discorrd.js

--- discorrd.js
+++ discorrd.js
@@ -1,12 +1,11 @@
-const { WebhookClient } = require('discord.js');
+const axios = require('axios');
 
 // Vervang de onderstaande URL door je eigen Discord-webhook URL
 const webhookURL = 'https://discord.com/api/webhooks/1219008140159352963/I07kOVhmJivstq-uZl3ccjvyHrjrnypKiULh73NM7WCcJRF3An7gslX7tR1Tn2_I8-ZL';
 
-// Maak een nieuwe WebhookClient aan
-const webhookClient = new WebhookClient({ url: webhookURL });
-
-// Verstuur een bericht naar de webhook
-webhookClient.send('hoi')
-  .then(() => console.log('Bericht succesvol verstuurd naar de webhook'))
-  .catch(console.error);
+// Maak een HTTP-verzoek naar de webhook om een bericht te sturen
+axios.post(webhookURL, {
+  content: 'hoi'
+})
+.then(() => console.log('Bericht succesvol verstuurd naar de webhook'))
+.catch(error => console.error('Er is een fout opgetreden bij het versturen van het bericht naar de webhook:', error));
